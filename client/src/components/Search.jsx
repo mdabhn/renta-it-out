fce363e9475aa1d66f1f09ab0509334b644c0136
Hossain <md.abhn@gmail.com>
2022-08-23 14:27:45 +0600

    :client: added Search component.

---
+++ client/src/components/Search.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Search = () => {
+  return (
+    <div>
+      <input
+        type='text'
+        className='border border-gray-400 rounded p-2 text-sm'
+        placeholder='Search'
+      />
+    </div>
+  )
+}
+
+export default Search
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import Search from '../components/Search'
 
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <div className='flex items-center flex-col'>
+      <h1 className='text-xl mt-10'>Renteal Platform</h1>
+      <div className='flex justify-end w-screen'>
+        <div className='p-10'>
+          <Search />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Home
